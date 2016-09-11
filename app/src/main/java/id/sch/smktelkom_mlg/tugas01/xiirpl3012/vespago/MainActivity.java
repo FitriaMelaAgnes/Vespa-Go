1b7e078fb2c2627dbe3057468b3528c9b8ccca60
Fitria Mela Agnes <fitria.mela9a17@gmail.com>
2016-09-11 23:03:00 +0700

    Penyelesaian pengkodean kondisi

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3012/vespago/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl3012/vespago/MainActivity.java
@@ -3,10 +3,13 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl3012.vespago;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     Spinner spKelas;
     RadioGroup rgArah;
     CheckBox cbH, cbM, cbS;
+    TextView tvNama, tvKelas, tvArah, tvAlat;
+    Button bOk;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,25 +31,75 @@ public class MainActivity extends AppCompatActivity {
         cbH = (CheckBox) findViewById(R.id.checkBoxH);
         cbM = (CheckBox) findViewById(R.id.checkBoxM);
         cbS = (CheckBox) findViewById(R.id.checkBoxSt);
+        tvNama = (TextView) findViewById(R.id.textViewNama);
+        tvKelas = (TextView) findViewById(R.id.textViewPc);
+        tvArah = (TextView) findViewById(R.id.textViewAt);
+        tvAlat = (TextView) findViewById(R.id.textViewPr);
+        bOk = (Button) findViewById(R.id.buttonOK);
 
-        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+        bOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                doClick();
+                doOk();
+                doAlat();
+                doArah();
+                doKelas();
             }
 
-            private void doClick() {
+            private void doKelas() {
+                tvKelas.setText("Paket Class Anda " + spKelas.getSelectedItem().toString());
+            }
 
-                String hasil = null
-                if (etNama.getText() == "" && spKelas.getSelectedItem() == "" && rgArah.getCheckedRadioButtonId() != -1 &&
-                        cbH.isChecked() == "" && cbM.isChecked() == "" && cbS.isChecked() == "") {
-                    hasil += "Anda belum mengisi data pemesanan";
-                } else {
-                    hasil += "Transaksi Anda sedang di proses";
+            private void doArah() {
+                String hasil = null;
+                if (rgArah.getCheckedRadioButtonId() != -1) {
+                    RadioButton rb = (RadioButton) findViewById(rgArah.getCheckedRadioButtonId());
+                    hasil = rb.getText().toString();
                 }
+                if (hasil == null) {
+                    tvArah.setText("Belum memilih arah tujuan");
+                } else {
+                    tvArah.setText("Tujuan Anda : " + hasil + "\n");
+                }
+            }
 
+            private void doAlat() {
+                String alat = "Peralatan keamanan yang anda pakai : \n";
+                int startlen = alat.length();
+                if (cbM.isChecked())
+                    alat += cbM.getText() + "\n";
+                if (cbH.isChecked())
+                    alat += cbH.getText() + "\n";
+                if (cbS.isChecked())
+                    alat += cbS.getText() + "\n";
+
+                if (alat.length() == startlen)
+                    alat += "Tidak ada di pilihan";
+
+                tvAlat.setText(alat);
+            }
+
+            private void doOk() {
+                if (isValid()) {
+                    String namaku = etNama.getText().toString();
+                    tvNama.setText("Nama Anda adalah " + namaku + "\n");
+                }
+            }
+
+            private boolean isValid() {
+                boolean valid = true;
+
+                String namaku = etNama.getText().toString();
+                if (namaku.isEmpty()) {
+                    etNama.setError("Nama belum diisi");
+                    valid = false;
+                } else {
+                    etNama.setError(null);
+                }
+                return valid;
             }
         });
+
 
     }
 }
